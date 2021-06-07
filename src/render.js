f55fc1d7d484dbfc03f89803ab652c6b2237f989
RyanLaufsen <ryan.y@shaw.ca>
2021-06-07 05:16:32 -0700

    v0.0.1

---
+++ src/render.js
@@ -0,0 +1,91 @@
+// Required modules
+const { remote } = require('electron');
+const { dialog } = remote;
+const leagueConnect = require('league-connect');
+
+// Custom utility functions
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Buttons
+const boostButton = document.getElementById('boostButton');
+boostButton.onclick = e => {
+    aramBoost();
+}
+
+// Display events
+// options = {
+//     message: 'League of Legends client is not running!',
+//     type: 'error',
+//     buttons: ['Okay.', 'Okay. I know I\'m a dumbass.'],
+//     defaultId: 1,
+//     title: 'Failed to BOOOST :(',
+//     detail: 'You didn\'t open the client yet.',
+//     // icon: 'todo'
+// }
+
+function handleClientNotFound() {
+    // document.getElementById('messages').style.opacity = 1;
+    // document.getElementById('messages').innerHTML = 'League of Legends <br> is not running';
+    dialog.showErrorBox('Failed to BOOOST :(','League of Legends client is not running!');
+    // dialog.showMessageBox(options);
+}
+
+function handleNotAram() {
+    dialog.showErrorBox('Failed to BOOOST :(','Not in ARAM game!');
+}
+
+async function handleBoost() {
+    // document.getElementById('messages').style.opacity = 1;
+    // document.getElementById('messages').style.fontWeight = 700;
+    // document.getElementById('messages').style.fontSize = '80px';
+    // document.getElementById('messages').style.color = 'var(--clr-neon)';
+    // document.getElementById('messages').innerHTML = 'BBBOOOSTED';
+    boostButton.innerText = 'BOOOSTED';
+    await sleep(5000);
+    boostButton.innerText = 'BOOOST';
+}
+
+// LCU API
+async function getCredentials() {
+    try {
+        credentials = await leagueConnect.authenticate()
+        return credentials;
+    } catch (e) {
+        console.error(e);
+        handleClientNotFound();
+    }
+}
+
+async function checkAram() {
+    try {
+        const response = await leagueConnect.request({
+            method: 'GET',
+            url: 'lol-champ-select/v1/session'
+        }, await getCredentials());
+        responseJSON = await response.json();
+        if (responseJSON.allowBattleBoost == true) {
+            return true;
+        } else {
+            handleNotAram();
+        };
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function aramBoost() {
+    if (await checkAram()) {
+        try {
+            const response = await leagueConnect.request({
+                method: 'POST',
+                url: '/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","activateBattleBoostV1",""]',
+            }, await getCredentials());
+            console.log("Response: " + response.statusText, response.status, "\nBOOOSTED");
+            handleBoost();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
