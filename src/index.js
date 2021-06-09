ed3e2aba106611875799fa12502162ef0d709e6b
RyanLaufsen <ryan.y@shaw.ca>
2021-06-09 03:46:27 -0700

    added service enabled verification with API call

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const { app, BrowserWindow, dialog, globalShortcut } = require('electron');
+const { app, BrowserWindow, dialog, globalShortcut, net } = require('electron');
 const path = require('path');
 require('@electron/remote/main').initialize();
+const fetch = require('electron-fetch').default;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
+
+async function checkEnabled() {
+  const response = await fetch('https://ryanlaufsen.github.io/data/booosted.json', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  });
+  responseJSON = await response.json();
+  return responseJSON.enabled;
+};
+
 
 const createWindow = () => {
   // Create the browser window.
@@ -35,7 +48,21 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', async () => {
+  if (await checkEnabled() === true) {
+    createWindow();
+  } else {
+    dialog.showMessageBoxSync({
+      title: 'NOOOOOOOOOOOOOOO!',
+      message: 'With a heavy heart, we must inform you:',
+      type: 'info',
+      buttons: ['Aw, man!', 'It\'s okay. I still love you.'],
+      defaultId: 1,
+      detail: 'The BOOOSTED ARAM Skin Boost service is not available at the moment. :(',
+      icon: __dirname + '/resources/fire.ico'
+    });
+  }
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -55,16 +82,16 @@ app.on('activate', () => {
 });
 
 // Prevents opening the dev console with Control+Shift+I
-// app.on('ready', () => {
-//   globalShortcut.register('Control+Shift+I', () => {
-//     options = {
-//       message: 'Did you press CTRL-SHIFT-I by accident? Sure you did. Gotcha >:(',
-//       type: 'warning',
-//       buttons: ['Aw, man!'],
-//       title: '!! HACKERMAN ALERT !!',
-//       detail: 'Your evil plots have been foiled!',
-//     }
-//     dialog.showMessageBox(options);
-//     return false;
-//   });
-// });
+app.on('ready', () => {
+  globalShortcut.register('Control+Shift+I', () => {
+    options = {
+      message: 'Did you press CTRL-SHIFT-I by accident? Sure you did. Gotcha >:(',
+      type: 'warning',
+      buttons: ['Aw, man!'],
+      title: '!! HACKERMAN ALERT !!',
+      detail: 'Your evil plots have been foiled!',
+    }
+    dialog.showMessageBox(options);
+    return false;
+  });
+});
